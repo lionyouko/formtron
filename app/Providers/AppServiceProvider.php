8975d70b0101c170f54e16fae283a739792aa314
lion-temp <liontemp@MacBook-12-lion.local>
2020-09-18 03:27:03 +0100

    ble

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,10 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-<<<<<<< HEAD
 use Illuminate\Support\Facades\Schema;
-=======
->>>>>>> 5a998a0445783be12403f43d8d03ccf5cc680b17
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,16 +12,11 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-<<<<<<< HEAD
     public function register(){
         //add this
         Schema::defaultStringLength(191);
-=======
-    public function register()
-    {
-        //
->>>>>>> 5a998a0445783be12403f43d8d03ccf5cc680b17
     }
+
 
     /**
      * Bootstrap any application services.
